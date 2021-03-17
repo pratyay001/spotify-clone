1c6b8cf248afbc92fbd95fc82c12842cca219440
pratyay001 <62806318+pratyay001@users.noreply.github.com>
2021-03-18 01:31:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Login from './login';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from './Dashboard';
+
+const code = new URLSearchParams(window.location.search).get("code")
+
+function App() {
+  return code ? <Dashboard code={code} /> : <Login />
+    
+     
+
+}
+
+export default App;
---
+++ src/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {Container} from 'react-bootstrap';
+const AUTH_URL ="https://accounts.spotify.com/authorize?client_id=b8ee9c6e1a96452887be6037f89f4edb&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+export default function login() {
+    return (
+        <Container className="d-flex justify-content-center align-items-center"
+        style={{ minHeight : "100vh"}}>
+            <a className="btn btn-success btn-lg" herf={AUTH_URL}>Login with spotify </a>
+        </Container> 
+            
+        
+    )
+}
